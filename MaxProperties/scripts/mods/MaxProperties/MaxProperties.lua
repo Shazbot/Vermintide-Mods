52b6336ea652ecb0cabf33b3d54d64b6d4753847
Unknown <tilic5@gmail.com>
2018-06-02 21:22:07 +0200

    added MaxProperties mod

---
+++ MaxProperties/scripts/mods/MaxProperties/MaxProperties.lua
@@ -0,0 +1,49 @@
+local mod = get_mod("MaxProperties") -- luacheck: ignore get_mod
+
+-- luacheck: globals BuffTemplates WeaponProperties WeaponTraits
+
+mod:hook("GearUtils.get_property_and_trait_buffs", function(func, backend_items, backend_id, buffs_table) -- luacheck: ignore func
+	local properties = backend_items:get_properties(backend_id)
+
+	if properties then
+		for property_key, property_value in pairs(properties) do -- luacheck: ignore property_value
+			local property_data = WeaponProperties.properties[property_key]
+			local buff_name = property_data.buff_name
+			local buffer = property_data.buffer or "client"
+
+			if BuffTemplates[buff_name] then
+				buffs_table[buffer][buff_name] = {
+					variable_value = 1 --property_value
+				}
+			end
+		end
+	end
+
+	local traits = backend_items:get_traits(backend_id)
+
+	if traits then
+		for _, trait_key in pairs(traits) do
+			local trait_data = WeaponTraits.traits[trait_key]
+			local buff_name = trait_data.buff_name
+			local buffer = trait_data.buffer or "client"
+
+			if BuffTemplates[buff_name] then
+				buffs_table[buffer][buff_name] = {
+					variable_value = 1
+				}
+			end
+		end
+	end
+
+	return buffs_table
+end)
+
+--- Callbacks ---
+mod.on_disabled = function(is_first_call) -- luacheck: ignore is_first_call
+	mod:disable_all_hooks()
+end
+
+mod.on_enabled = function(is_first_call) -- luacheck: ignore is_first_call
+	mod:enable_all_hooks()
+end
+
---
+++ MaxProperties/scripts/mods/MaxProperties/MaxProperties_data.lua
@@ -0,0 +1,6 @@
+local mod = get_mod("MaxProperties") -- luacheck: ignore get_mod
+
+return {
+	name = "MaxProperties",
+	description = mod:localize("mod_description"),
+}

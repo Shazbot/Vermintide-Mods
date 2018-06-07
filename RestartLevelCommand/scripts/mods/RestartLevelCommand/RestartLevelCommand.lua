4ce1a276bc40ade608459dba1e68ad91d1277b98
Unknown <tilic5@gmail.com>
2018-06-07 16:27:05 +0200

    added RestartLevelCommand mod

---
+++ RestartLevelCommand/scripts/mods/RestartLevelCommand/RestartLevelCommand.lua
@@ -0,0 +1,36 @@
+local mod = get_mod("RestartLevelCommand") -- luacheck: ignore get_mod
+
+-- luacheck: globals Managers
+
+mod.do_insta_fail = false
+
+mod.restart_level = function(self) -- luacheck: ignore self
+	mod:pcall(function()
+		if Managers.state.game_mode:level_key() == "inn_level" then
+			mod:echo("Can't restart in the keep.")
+			return
+		end
+
+		mod.do_insta_fail = true
+		Managers.state.game_mode:fail_level()
+	end)
+end
+
+mod:hook("GameModeAdventure.evaluate_end_conditions", function(func, self, round_started, dt, t)
+	local restart = false
+	if self.lost_condition_timer and mod.do_insta_fail then
+		mod.do_insta_fail = false
+		self.lost_condition_timer = t - 1
+		restart = true
+	end
+
+	local ended, reason = func(self, round_started, dt, t)
+
+	if ended and restart then
+		return ended, "reload"
+	end
+
+	return ended, reason
+end)
+
+mod:command("restart", mod:localize("restart_level_command_description"), function() mod:restart_level() end)
---
+++ RestartLevelCommand/scripts/mods/RestartLevelCommand/RestartLevelCommand_localization.lua
@@ -0,0 +1,11 @@
+return {
+	mod_name = {
+		en = "Restart Command"
+	},
+	mod_description = {
+		en = "/restart to restart the map."
+	},
+	restart_level_command_description = {
+		en = "Restart the level."
+	},
+}

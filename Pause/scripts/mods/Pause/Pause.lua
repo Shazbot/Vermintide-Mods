ace1815c2913a72e970eb7cf9ed358895a331c79
Unknown <tilic5@gmail.com>
2018-08-06 10:39:50 +0200

    added Pause mod

---
+++ Pause/scripts/mods/Pause/Pause.lua
@@ -0,0 +1,20 @@
+local mod = get_mod("Pause") -- luacheck: ignore get_mod
+
+-- luacheck: globals Managers
+
+mod.do_pause = function()
+	if not Managers.player.is_server then
+		mod:echo(mod:localize("not_server"))
+		return
+	end
+
+	if Managers.state.debug.time_paused then
+		Managers.state.debug:set_time_scale(Managers.state.debug.time_scale_index)
+		mod:echo(mod:localize("game_unpaused"))
+	else
+		Managers.state.debug:set_time_paused()
+		mod:echo(mod:localize("game_paused"))
+	end
+end
+
+mod:command("pause", mod:localize("pause_command_description"), mod.do_pause)

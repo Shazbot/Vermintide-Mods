8c025d06a926958990edb9d843b1e6724f27e62b
Unknown <tilic5@gmail.com>
2018-08-17 20:26:16 +0200

    VisibleAmmo: make it work with controller

--- VisibleAmmo/scripts/mods/VisibleAmmo/VisibleAmmo.lua
+++ VisibleAmmo/scripts/mods/VisibleAmmo/VisibleAmmo.lua
@@ -1,11 +1,19 @@
-local mod = get_mod("VisibleAmmo")
+local mod = get_mod("VisibleAmmo") -- luacheck: ignore get_mod
+
+-- luacheck: globals EquipmentUI GamePadEquipmentUI
 
 --- Hooks ---
-mod:hook(EquipmentUI, "_animate_ammo_counter", function (func, self, ...)
+mod._animate_ammo_counter_hook = function (func, self, ...)
 	self._ammo_counter_fade_delay = math.huge
 	return func(self, ...)
-end)
+end
 
-mod:hook(EquipmentUI, "_set_ammo_text_focus", function (func, self, focus) -- luacheck: ignore focus
+mod:hook(EquipmentUI, "_animate_ammo_counter", mod._animate_ammo_counter_hook)
+mod:hook(GamePadEquipmentUI, "_animate_ammo_counter", mod._animate_ammo_counter_hook)
+
+mod._set_ammo_text_focus_hook = function (func, self, focus) -- luacheck: ignore focus
 	return func(self, true)
-end)
+end
+
+mod:hook(EquipmentUI, "_set_ammo_text_focus", mod._set_ammo_text_focus_hook)
+mod:hook(GamePadEquipmentUI, "_set_ammo_text_focus", mod._set_ammo_text_focus_hook)

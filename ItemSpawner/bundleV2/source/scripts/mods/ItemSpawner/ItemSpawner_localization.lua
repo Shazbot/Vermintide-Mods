5a4dfb405cc356302031a3fb8ee354c202b7c82e
Unknown <tilic5@gmail.com>
2018-09-12 00:19:53 +0200

    ItemSpawner: remember pickup between mod reloads

---
+++ ItemSpawner/bundleV2/source/scripts/mods/ItemSpawner/ItemSpawner_localization.lua
@@ -0,0 +1,32 @@
+return {
+	mod_name = {
+		en = "Item Spawner"
+	},
+	mod_description = {
+		en = "Spawn Items"
+	},
+	next_pickup_hotkey = {
+		en = "Next Item"
+	},
+	next_pickup_hotkey_tooltip = {
+		en = "Cycle to the next item."
+	},
+	prev_pickup_hotkey = {
+		en = "Previous Item"
+	},
+	prev_pickup_hotkey_tooltip = {
+		en = "Cycle to the previous item."
+	},
+	spawn_pickup_hotkey = {
+		en = "Spawn Item"
+	},
+	spawn_pickup_hotkey_tooltip = {
+		en = "Spawns the item beneath your feet."
+	},
+	switch_item_command_description = {
+		en = "Switch to another item. e.g. /switch cooldown"
+	},
+	keybinds_group = {
+		en = "Per Item Keybinds"
+	},
+}

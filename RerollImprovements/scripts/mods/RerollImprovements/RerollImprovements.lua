25fa4d921e409bfa966335ca6d8301ab3738c657
Unknown <tilic5@gmail.com>
2018-09-13 20:40:46 +0200

    RerollImprovements: added trait rerolling support

--- RerollImprovements/scripts/mods/RerollImprovements/RerollImprovements.lua
+++ RerollImprovements/scripts/mods/RerollImprovements/RerollImprovements.lua
@@ -1,7 +1,8 @@
 local mod = get_mod("RerollImprovements") -- luacheck: ignore get_mod
 
  -- luacheck: globals CraftPageRollProperties Managers UIUtils Managers UIWidgets
- -- luacheck: globals UIWidget Colors UISettings
+ -- luacheck: globals UIWidget Colors UISettings WeaponTraits CraftPageRollTrait
+ -- luacheck: globals Localize
 
 UISettings.crafting_progress_time = 0.01
 
@@ -26,15 +27,15 @@ mod.property_text_style = {
 
 mod:hook_safe(CraftPageRollProperties, "_add_craft_item", function(self, backend_id)
 	mod:pcall(function()
-		if self._craft_items[1] and self._widgets_by_name["properties_1"] then
+		if self._craft_items[1] and self._widgets_by_name["mod_properties_1"] then
 			local properties = Managers.backend:get_interface("items"):get_properties(self._craft_items[1])
 
-			self._widgets_by_name["properties_1"].content.text = ""
-			self._widgets_by_name["properties_2"].content.text = ""
+			self._widgets_by_name["mod_properties_1"].content.text = ""
+			self._widgets_by_name["mod_properties_2"].content.text = ""
 
 			local index = 1
 			for prop_name, prop_value in pairs( properties ) do
-				self._widgets_by_name["properties_"..index].content.text = UIUtils.get_property_description(prop_name, prop_value)
+				self._widgets_by_name["mod_properties_"..index].content.text = UIUtils.get_property_description(prop_name, prop_value)
 				index = index + 1
 			end
 		end
@@ -43,14 +44,18 @@ mod:hook_safe(CraftPageRollProperties, "_add_craft_item", function(self, backend
 end)
 
 mod:hook_safe(CraftPageRollProperties, "_remove_craft_item", function(self)
-	if not self._craft_items[1] and self._widgets_by_name["properties_1"] then
-		self._widgets_by_name["properties_1"].content.text = ""
-		self._widgets_by_name["properties_2"].content.text = ""
+	if not self._craft_items[1] and self._widgets_by_name["mod_properties_1"] then
+		self._widgets_by_name["mod_properties_1"].content.text = ""
+		self._widgets_by_name["mod_properties_2"].content.text = ""
+		mod.last_backend_id = nil
 	end
 end)
 
 mod:hook_safe(CraftPageRollProperties, "on_craft_completed", function(self)
-	self:_add_craft_item(mod.last_backend_id)
+	local item_interface = Managers.backend:get_interface("items")
+	if item_interface:get_item_from_id(mod.last_backend_id) then
+		self:_add_craft_item(mod.last_backend_id)
+	end
 end)
 
 mod:hook_safe(CraftPageRollProperties, "create_ui_elements", function(self)
@@ -58,11 +63,56 @@ mod:hook_safe(CraftPageRollProperties, "create_ui_elements", function(self)
 	local widgets_by_name = self._widgets_by_name
 
 	for i = 1, 2 do
-		local text_definition = UIWidgets.create_simple_text("", "craft_bar", nil, nil, mod.property_text_style)
+		local text_definition = UIWidgets.create_simple_text("", "recipe_grid", nil, nil, mod.property_text_style)
 		local widget = UIWidget.init(text_definition)
 		widgets[#widgets + 1] = widget
-		widgets_by_name["properties_"..i] = widget
-		widget.offset[1] = 80 + 120
-		widget.offset[2] = (i-1)*(-25) + 230
+		widgets_by_name["mod_properties_"..i] = widget
+		widget.offset[1] = 0
+		widget.offset[2] = 87+(i-1)*(-26)
+		widget.offset[3] = 50
 	end
+end)
+
+--- Trait Rerolling ---
+mod:hook_safe(CraftPageRollTrait, "_add_craft_item", function(self, backend_id)
+	mod:pcall(function()
+		if self._craft_items[1] and self._widgets_by_name["mod_trait"] then
+			local traits = Managers.backend:get_interface("items"):get_traits(self._craft_items[1])
+
+			self._widgets_by_name["mod_trait"].content.text = ""
+
+			local trait = traits[1]
+			if trait then
+				self._widgets_by_name["mod_trait"].content.text = Localize(WeaponTraits.traits[trait].display_name)
+			end
+		end
+		mod.last_backend_id = backend_id
+	end)
+end)
+
+mod:hook_safe(CraftPageRollTrait, "_remove_craft_item", function(self)
+	if not self._craft_items[1] and self._widgets_by_name["mod_trait"] then
+		self._widgets_by_name["mod_trait"].content.text = ""
+		mod.last_backend_id = nil
+	end
+end)
+
+mod:hook_safe(CraftPageRollTrait, "on_craft_completed", function(self)
+	local item_interface = Managers.backend:get_interface("items")
+	if item_interface:get_item_from_id(mod.last_backend_id) then
+		self:_add_craft_item(mod.last_backend_id)
+	end
+end)
+
+mod:hook_safe(CraftPageRollTrait, "create_ui_elements", function(self)
+	local widgets = self._widgets
+	local widgets_by_name = self._widgets_by_name
+
+	local text_definition = UIWidgets.create_simple_text("", "recipe_grid", nil, nil, mod.property_text_style)
+	local widget = UIWidget.init(text_definition)
+	widgets[#widgets + 1] = widget
+	widgets_by_name["mod_trait"] = widget
+	widget.offset[1] = -5
+	widget.offset[2] = 73
+	widget.offset[3] = 50
 end)

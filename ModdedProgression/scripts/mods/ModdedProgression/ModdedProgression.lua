7d6eae4fb3f56dddbb92441c1a3c75a0ff1a4880
Unknown <tilic5@gmail.com>
2019-05-27 17:12:51 +0200

    MP: now track lower difficulties for mutators

--- ModdedProgression/scripts/mods/ModdedProgression/ModdedProgression.lua
+++ ModdedProgression/scripts/mods/ModdedProgression/ModdedProgression.lua
@@ -5,21 +5,46 @@ local pl = require'pl.import_into'()
 local vmf = get_mod("VMF")
 
 mod:hook_safe(StatisticsUtil, "_register_completed_level_difficulty", function(_, level_id, _, difficulty_name)
-	if difficulty_name ~= "hardest" then
-		return
-	end
-
 	local flush_settings = false
 
+	local dw_enabled, ons_enabled = false, false
 	local dwons_mod = get_mod("is-dwons-on")
 	if dwons_mod then
-		local dw_enabled, ons_enabled = dwons_mod.get_status()
-		local completions_lookup = {
-			ONS_COMPLETION = ons_enabled,
-			DW_COMPLETION = dw_enabled,
-			DWONS_COMPLETION = dw_enabled and ons_enabled,
-		}
+		dw_enabled, ons_enabled = dwons_mod.get_status()
+	end
 
+	local completions_lookup = {
+		ONS_COMPLETION = ons_enabled,
+		DW_COMPLETION = dw_enabled,
+		DWONS_COMPLETION = dw_enabled and ons_enabled,
+	}
+
+	local difficulty_done_lookup = {
+		ONS_DIFFICULTY = ons_enabled,
+		DW_DIFFICULTY = dw_enabled,
+		DWONS_DIFFICULTY = dw_enabled and ons_enabled,
+	}
+
+	local difficulty_index = pl.tablex.find(DefaultDifficulties, difficulty_name)
+	if difficulty_index then
+		for completion_key, enabled in pairs( difficulty_done_lookup ) do
+			if enabled then
+				local completions = mod:get(completion_key) or {}
+				if completions[level_id] then
+					local done_difficulty_index = pl.tablex.find(DefaultDifficulties, completions[level_id])
+					if done_difficulty_index and done_difficulty_index > difficulty_index then
+						completions[level_id] = difficulty_name
+					end
+				else
+					completions[level_id] = difficulty_name
+				end
+				mod:set(completion_key, completions)
+				flush_settings = true
+			end
+		end
+	end
+
+	if difficulty_name == "hardest" then
 		for completion_key, enabled in pairs( completions_lookup ) do
 			if enabled then
 				local completions = mod:get(completion_key) or {}
@@ -28,20 +53,20 @@ mod:hook_safe(StatisticsUtil, "_register_completed_level_difficulty", function(_
 				flush_settings = true
 			end
 		end
-	end
 
-	if mod.map_start_time then
-		mod.map_time = Managers.time:time("game") - mod.map_start_time
-		if mod.map_time > 60 then
-			local map_times = mod:get("MAP_TIMES") or {}
-			map_times[level_id] = map_times[level_id] or {}
-			map_times[level_id] =
-				pl.List(map_times[level_id])
-				:append(mod.map_time)
-				:sorted()
-				:slice(1,10)
-			mod:set("MAP_TIMES", map_times)
-			flush_settings = true
+		if mod.map_start_time then
+			mod.map_time = Managers.time:time("game") - mod.map_start_time
+			if mod.map_time > 60 then
+				local map_times = mod:get("MAP_TIMES") or {}
+				map_times[level_id] = map_times[level_id] or {}
+				map_times[level_id] =
+					pl.List(map_times[level_id])
+					:append(mod.map_time)
+					:sorted()
+					:slice(1,10)
+				mod:set("MAP_TIMES", map_times)
+				flush_settings = true
+			end
 		end
 	end
 
@@ -70,6 +95,21 @@ mod:hook_safe(StartGameWindowMissionSelection, "_present_acts", function(self)
 	end
 end)
 
+mod.get_map_time = function(level_id)
+	local map_times = mod:get("MAP_TIMES") or {}
+
+	local map_time = "00:00"
+
+	if map_times[level_id] and map_times[level_id][1] then
+		local total_secs = map_times[level_id][1]
+		local mins = math.floor(total_secs/60)
+		local secs = math.round(total_secs % 60)
+		map_time = mins..":"..secs
+	end
+
+	return map_time
+end
+
 mod:hook(StartGameWindowMissionSelection, "update", function(func, self, dt, t)
 	mod:pcall(function()
 		self._widgets_by_name.selected_level.offset[1] = 0
@@ -83,27 +123,39 @@ mod:hook(StartGameWindowMissionSelection, "update", function(func, self, dt, t)
 
 		local level_id = self._selected_level_id or ""
 
-		local map_times = mod:get("MAP_TIMES") or {}
+		local modded_mut_to_difficutly_done_setting_lookup = pl.Map{
+			Onslaught = "ONS_DIFFICULTY",
+			Deathwish = "DW_DIFFICULTY",
+			DwOns = "DWONS_DIFFICULTY",
+		}
 
-		local map_time = "00:00"
+		local modded_mut_to_done_on_hardest_lookup = pl.Map{
+			Onslaught = mod:get("ONS_COMPLETION") or {},
+			Deathwish = mod:get("DW_COMPLETION") or {},
+			DwOns = mod:get("DWONS_COMPLETION") or {},
+		}
 
-		if map_times[level_id] and map_times[level_id][1] then
-			local total_secs = map_times[level_id][1]
-			local mins = math.floor(total_secs/60)
-			local secs = math.round(total_secs % 60)
-			map_time = mins..":"..secs
+		local info_out = {}
+		for modded_mut, done_on_difficulty_setting in pairs( modded_mut_to_difficutly_done_setting_lookup ) do
+			local done_on_difficulty = mod:get(done_on_difficulty_setting) or {}
+			local done_difficulty = done_on_difficulty[level_id]
+			if modded_mut_to_done_on_hardest_lookup[modded_mut][level_id] then
+				info_out[modded_mut] = "Yes"
+			elseif not done_difficulty then
+				info_out[modded_mut] = "No"
+			elseif done_difficulty == "hardest" then
+				info_out[modded_mut] = "Yes"
+			else
+				info_out[modded_mut] = Localize(DifficultySettings[done_difficulty].display_name)
+			end
 		end
 
-		local ons_completion = mod:get("ONS_COMPLETION") or {}
-		local dw_completion = mod:get("DW_COMPLETION") or {}
-		local dwons_completion = mod:get("DWONS_COMPLETION") or {}
-
-		self.mod_progression_text.content.text = pl.List{
-			"Time: "..map_time,
-			string.format("Onslaught: %s", ons_completion[level_id] and "Yes" or "No"),
-			string.format("Deathwish: %s", dw_completion[level_id] and "Yes" or "No"),
-			string.format("DwOns: %s", dwons_completion[level_id] and "Yes" or "No"),
-		}:join("\n")
+		self.mod_progression_text.content.text =
+			modded_mut_to_difficutly_done_setting_lookup:keys():map(function(modded_mut)
+				return string.format("%s: %s", modded_mut, info_out[modded_mut])
+			end)
+			:insert(1, "Time: "..mod.get_map_time(level_id))
+			:join("\n")
 	end)
 
 	return func(self, dt, t)

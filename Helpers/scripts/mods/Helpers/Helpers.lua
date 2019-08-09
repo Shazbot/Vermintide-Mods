7b57042bdfc57fabbba7ab407e72e22b9519efb9
Tibor Ilić <tilic5@gmail.com>
2019-08-09 12:58:52 +0200

    Helpers: fixed ult reset

--- Helpers/scripts/mods/Helpers/Helpers.lua
+++ Helpers/scripts/mods/Helpers/Helpers.lua
@@ -5,7 +5,12 @@ mod.reset_ult = function()
 	local local_player_unit = Managers.player:local_player().player_unit
 	local career_extension = ScriptUnit.has_extension(local_player_unit, "career_system")
 	if career_extension then
-		career_extension._cooldown = 0
+		local abilities = career_extension._abilities
+
+		for i = 1, career_extension._num_abilities, 1 do
+			local ability = abilities[i]
+			ability.cooldown = 0
+		end
 	end
 end
 

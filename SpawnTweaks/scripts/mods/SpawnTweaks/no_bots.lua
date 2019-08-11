6c5ca8971d8783edeed902202d152b082d8d325a
Tibor Ilić <tilic5@gmail.com>
2019-08-11 22:29:39 +0200

    fixed bot disabling with 2.0

--- SpawnTweaks/scripts/mods/SpawnTweaks/no_bots.lua
+++ SpawnTweaks/scripts/mods/SpawnTweaks/no_bots.lua
@@ -1,44 +1,15 @@
 local mod = get_mod("SpawnTweaks")
 
 --- Disable the bots.
-mod:hook(SpawnManager, "_update_bot_spawns", function(func, ...)
+mod:hook(GameModeAdventure, "_handle_bots",
+function(func, self, ...)
 	local original_cap_num_bots = script_data.cap_num_bots
 
 	if mod:get(mod.SETTING_NAMES.NO_BOTS) then
 		script_data.cap_num_bots = 0
 	end
 
-	func(...)
+	func(self, ...)
 
 	script_data.cap_num_bots = original_cap_num_bots
-end)
-
---- Fix for bug with mission not ending when bots are disabled.
-mod:hook(SpawnManager, "all_players_disabled", function(func, self, ...)
-	if not mod:get(mod.SETTING_NAMES.NO_BOTS) then
-		return func(self, ...)
-	end
-
-	-- fix the auto-fail on map start when with other people
-	local game_mode_manager = Managers.state.game_mode
-	if not game_mode_manager then
-		return false
-	end
-
-	local round_started = game_mode_manager:is_round_started()
-	if not round_started then
-		for _, status in ipairs(self._player_statuses) do
-			if status.spawn_state == "initial_spawning" then
-				return false
-			end
-		end
-	end
-
-	for _, status in ipairs(self._player_statuses) do
-		if status.health_state == "alive" and status.spawn_state == "spawned" then
-			return false
-		end
-	end
-
-	return true
 end)
--- TrueSoloQoL/scripts/mods/TrueSoloQoL/TrueSoloQoL.lua
+++ TrueSoloQoL/scripts/mods/TrueSoloQoL/TrueSoloQoL.lua
@@ -93,46 +93,17 @@ mod.update = function()
 end
 
 --- Disable the bots.
-mod:hook(SpawnManager, "_update_bot_spawns", function(func, ...)
+mod:hook(GameModeAdventure, "_handle_bots",
+function(func, self, ...)
 	local original_cap_num_bots = script_data.cap_num_bots
 
 	if mod:get(mod.SETTING_NAMES.AUTO_KILL_BOTS) then
 		script_data.cap_num_bots = 0
 	end
 
-	func(...)
+	func(self, ...)
 
 	script_data.cap_num_bots = original_cap_num_bots
-end)
-
---- Fix for bug with mission not ending when bots are disabled.
-mod:hook(SpawnManager, "all_players_disabled", function(func, self, ...)
-	if not mod:get(mod.SETTING_NAMES.AUTO_KILL_BOTS) then
-		return func(self, ...)
-	end
-
-	-- fix the auto-fail on map start when with other people
-	local game_mode_manager = Managers.state.game_mode
-	if not game_mode_manager then
-		return false
-	end
-
-	local round_started = game_mode_manager:is_round_started()
-	if not round_started then
-		for _, status in ipairs(self._player_statuses) do
-			if status.spawn_state == "initial_spawning" then
-				return false
-			end
-		end
-	end
-
-	for _, status in ipairs(self._player_statuses) do
-		if status.health_state == "alive" and status.spawn_state == "spawned" then
-			return false
-		end
-	end
-
-	return true
 end)
 
 mod:hook("Localize", function(func, key)

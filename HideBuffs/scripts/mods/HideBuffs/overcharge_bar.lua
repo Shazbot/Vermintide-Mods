6a4afa372946a77f2fe10655a49959ed1e603191
Tibor Ilić <tilic5@gmail.com>
2019-10-29 21:39:06 +0100

    HB: when showing ammo with heat, hide the sliver of heat bar left when at 0 ammo

--- HideBuffs/scripts/mods/HideBuffs/overcharge_bar.lua
+++ HideBuffs/scripts/mods/HideBuffs/overcharge_bar.lua
@@ -16,8 +16,9 @@ end)
 mod:hook(OverchargeBarUI, "_update_overcharge", function(func, self, player, dt)
 	local has_overcharge = func(self, player, dt)
 
-	-- restore max pip to default size if it was hidden before
+	-- reset the max pip and the main bar to default size so they're not hidden
 	self.charge_bar.style.max_threshold.size[2] = 10
+	self.charge_bar.style.bar_1.size[2] = 10
 
 	-- check if already using overcharge weapon
 	if has_overcharge then
@@ -91,6 +92,9 @@ mod:hook(OverchargeBarUI, "_update_overcharge", function(func, self, player, dt)
 	    self.charge_bar.content.internal_gradient_threshold = ammo_fraction
 	    self:set_charge_bar_fraction(ammo_fraction, min_threshold_fraction, max_threshold_fraction)
 	    self.charge_bar.style.max_threshold.size[2] = 0
+	    if ammo_count == 0 then
+				self.charge_bar.style.bar_1.size[2] = 0
+	    end
 	    return true
 	end
 end)

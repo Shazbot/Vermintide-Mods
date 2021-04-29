39eb150c53fb9960deabfd76cab415778e9a7b16
Tibor Ilić <tilic5@gmail.com>
2021-04-29 19:04:51 +0200

    MutatorSelector: fix for some of the new mutators missing a display_name

--- MutatorsSelector/scripts/mods/MutatorsSelector/MutatorsSelector_data.lua
+++ MutatorsSelector/scripts/mods/MutatorsSelector/MutatorsSelector_data.lua
@@ -7,6 +7,8 @@ local mod_data = {
 }
 
 mod:hook("Localize", function(func, id, ...)
+		if not id then return end
+
 		local localized = func(id, ...)
 
 		if string.find(localized, "<") == 1 then
@@ -22,14 +24,18 @@ end)
 mod_data.options_widgets = (function()
 	local mutator_checkboxes = {}
 	for mut_name, mut_template in pairs( MutatorTemplates ) do
-		table.insert(mutator_checkboxes,
-			{
-				["setting_name"] = mut_name,
-				["widget_type"] = "checkbox",
-				["text"] = Localize(mut_template.display_name),
-				["tooltip"] = Localize(mut_template.description),
-				["default_value"] = false
-			})
+		if mut_template.display_name and mut_template.description then
+			table.insert(
+				mutator_checkboxes,
+				{
+					["setting_name"] = mut_name,
+					["widget_type"] = "checkbox",
+					["text"] = Localize(mut_template.display_name or ""),
+					["tooltip"] = Localize(mut_template.description or ""),
+					["default_value"] = false
+				}
+			)
+		end
 	end
 	return mutator_checkboxes
 end)()
